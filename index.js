12e33fef1a0dc9ecc058400bec07f18912f6aebf
Ben <ben.vandenberg@carleton.ca>
2020-08-22 11:33:37 -0400

    add welcome message

--- index.js
+++ index.js
@@ -42,6 +42,16 @@ bot.on('message', message => {
 
 });
 
+// Create an event listener for new guild members
+bot.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 bot.on('shardError', error => {
   console.error('A websocket connection encountered an error:', error);
 });
