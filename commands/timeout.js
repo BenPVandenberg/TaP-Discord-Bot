5fa8c4986b7a304594b2b78bf0cc606fb96368cd
Ben <ben.vandenberg@carleton.ca>
2020-08-28 11:56:25 -0400

    timeout cmd init

---
+++ commands/timeout.js
@@ -0,0 +1,61 @@
+const config = require('../config.json');
+const ytdl = require('ytdl-core');
+// ranks.js
+// ========
+module.exports = {
+  name: 'timeout',
+  description: 'Moves the user to their own channel for 1 min',
+  execute(message, args) { // eslint-disable-line no-unused-vars
+    const timeout_config = config.commands.timeout;
+    const user_to_timeout = message.mentions.members.values().next().value;
+
+    // verify the user @'d someone
+    if (user_to_timeout === undefined) {
+      message.reply('usage is /timeout @user');
+      return;
+    }
+
+    const member_to_timeout = user_to_timeout.presence.member;
+    const original_channel = member_to_timeout.voice.channel;
+
+    // ensure member_to_stfu is in a voice channel
+    if (!original_channel) {
+      message.reply('user is not in a voice channel.');
+      return;
+    }
+
+    // find a channel to move user to
+    let eligible_channel;
+    const channels = message.guild.channels.cache;
+    for (const [key, channel] of channels.entries()) { // eslint-disable-line no-unused-vars
+      if (['Muahahahahahah'].includes(channel.name)) {
+        eligible_channel = channel;
+        break;
+      }
+    }
+
+    // verify we got a channel to move to
+    if (!eligible_channel) {
+      message.reply('there arn\'t any eligible channels atm.');
+    }
+
+    // the magic
+    message.react('👍');
+    member_to_timeout.voice.setChannel(eligible_channel);
+
+    // join and play yt audio
+    eligible_channel.join().then(connection => {
+      const audioLink = timeout_config.music;
+      const stream = ytdl(audioLink, { filter: 'audioonly' });
+      const dispatcher = connection.play(stream);
+
+      dispatcher.on('finish', () => {
+        // return member
+        member_to_timeout.voice.setChannel(original_channel);
+        eligible_channel.leave();
+      });
+    });
+
+    return;
+  },
+};
