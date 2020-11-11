3f5367d1de658efce83707d496cad0c915ceb201
Ben <ben.vandenberg@carleton.ca>
2020-11-11 16:13:43 -0500

    random color has a min brightness now

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.on('message', async message => {
   // update cheif of military tatics' role colour
   const role = message.guild.roles.cache.find(r => r.name === 'Cheif of Military Tactics');
   role.edit({
-    color: getRandomColor(),
+    color: getColor(),
   });
 
   // check if it is a bot command in a non command channel
@@ -78,13 +78,14 @@ bot.on('guildMemberAdd', member => {
   if (!channel) return;
   // Send the message, mentioning the member
 
-  const rules_string = `1. Be wary of the wild Greenlizid + Grim, they DO bite!
+  const rules_string = `
+1. Be wary of the wild Greenlizid + Grim, they DO bite!
 2. This is NOT a democrocy (RIP ur free will)
 3. The President is "not racist" -President
 4. Add game ranks by using /ranks
 BONUS: /play gbtm ;)`;
 
-  channel.send(`Welcome to the server, ${member}. \n` + rules_string);
+  channel.send(`Welcome to the server, ${member}.` + rules_string);
 });
 
 bot.on('shardError', error => {
@@ -97,12 +98,36 @@ process.on('unhandledRejection', error => {
 
 bot.login(process.env.DISCORD_LOGIN_TOKEN);
 
+function getColor() {
+  return hslToHex(360 * Math.random(), 100 * Math.random(), 30 + 70 * Math.random());
+}
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+function hslToHex(h, s, l) {
+  h /= 360;
+  s /= 100;
+  l /= 100;
+  let r, g, b;
+  if (s === 0) {
+    r = g = b = l; // achromatic
   }
-  return color;
+  else {
+    const hue2rgb = (p, q, t) => {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+  const toHex = x => {
+    const hex = Math.round(x * 255).toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  };
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
 }
