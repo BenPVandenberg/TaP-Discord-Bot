f8ff8b3014a4cb3a15118b5104fcd1a268012879
Ben <ben.vandenberg@carleton.ca>
2021-01-15 00:00:50 -0500

    .js to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+function WelcomeMessage() {
+  return <p>Welcome!</p>
+ }
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +13,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <WelcomeMessage />
         <a
           className="App-link"
           href="https://reactjs.org"
