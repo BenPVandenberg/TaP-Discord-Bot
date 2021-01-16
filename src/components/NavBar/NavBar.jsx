aee9e49ccb2a786943ca04adb9661ff3fdef8e23
Ben <ben.vandenberg@carleton.ca>
2021-01-15 21:28:00 -0500

    nav bar implemented

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,36 @@
-import logo from '../../assets/logo.svg'
 import './NavBar.css';
+import logo from '../../assets/logo.svg'
+import { FaHome, FaMusic } from "react-icons/fa"
+import { ImDatabase } from 'react-icons/im'
+import { BrowserRouter as Link } from 'react-router-dom'
 
-function navBar() {
+
+function NavBar() {
     return (
         <div className="navWrapper">
-            <img src={logo} className="App-logo" alt="logo" height="10px" />
+            <img src={logo} className="App-logo" alt="logo" />
+
+            <div className="navEntry">
+                <Link to={`/`}>
+                    <FaHome size={50} />
+                    <p>Home</p>
+                </Link>
+            </div>
+            <div className="navEntry">
+                <Link to={`/sounds`}>
+                    <FaMusic size={50} />
+                    <p>Sounds</p>
+                </Link>
+            </div>
+            <div className="navEntry">
+                <Link to={`/data`}>
+                    <ImDatabase size={50}/>
+                    <p>Data</p>
+                </Link>
+            </div>
+
         </div>
     );
 }
 
-export default navBar;
+export default NavBar;
