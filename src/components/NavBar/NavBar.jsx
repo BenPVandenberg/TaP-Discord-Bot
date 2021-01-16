46305078c1f0d05b228f92780207a16ecc77162a
Ben <ben.vandenberg@carleton.ca>
2021-01-16 00:12:40 -0500

    finalizing navBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,35 +2,36 @@ import './NavBar.css';
 import logo from '../../assets/logo.svg'
 import { FaHome, FaMusic } from "react-icons/fa"
 import { ImDatabase } from 'react-icons/im'
-import { BrowserRouter as Link } from 'react-router-dom'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 
-function NavBar() {
+function navBar() {
     return (
         <div className="navWrapper">
             <img src={logo} className="App-logo" alt="logo" />
 
-            <div className="navEntry">
+            <Router>
                 <Link to={`/`}>
-                    <FaHome size={50} />
-                    <p>Home</p>
+                    <div className="navEntry">
+                        <FaHome size={35} />
+                        <p>Home</p>
+                    </div>
                 </Link>
-            </div>
-            <div className="navEntry">
                 <Link to={`/sounds`}>
-                    <FaMusic size={50} />
-                    <p>Sounds</p>
+                    <div className="navEntry">
+                        <FaMusic size={35} />
+                        <p>Sounds</p>
+                    </div>
                 </Link>
-            </div>
-            <div className="navEntry">
                 <Link to={`/data`}>
-                    <ImDatabase size={50}/>
-                    <p>Data</p>
+                    <div className="navEntry">
+                        <ImDatabase size={35} />
+                        <p>Data</p>
+                    </div>
                 </Link>
-            </div>
-
+            </Router>
         </div>
     );
 }
 
-export default NavBar;
+export default navBar;
