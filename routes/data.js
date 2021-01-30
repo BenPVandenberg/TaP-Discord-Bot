088c63ff707db00ff6a16ce042f775a556a9bdb5
Ben <ben.vandenberg@carleton.ca>
2021-01-30 02:00:26 -0500

    Database integration

--- routes/data.js
+++ routes/data.js
@@ -2,13 +2,62 @@ const express = require('express');
 
 const router = express.Router();
 const fs = require('fs');
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: process.env.DB_ADDRESS,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'Discord_Bot',
+  timezone: 'EST',
+});
 
 /* GET data listing. */
 router.get('/:file', (req, res, next) => {
   // send contents of data file
   try {
-    const rawData = fs.readFileSync(`${process.env.DATA_DIR + req.params.file}.json`);
-    res.status(200).send(rawData);
+    const rtnDataDict = {};
+    const rtnDataArr = [];
+    switch (req.params.file) {
+      case 'play':
+        con.query(
+          'SELECT SoundName, Count(*) AS Occurrences FROM Discord_Bot.PlayLog GROUP BY SoundName;',
+          (err, result) => {
+            if (err) res.status(400).send({ msg: err.message });
+
+            result.forEach((element) => {
+              rtnDataDict[element.SoundName] = element.Occurrences;
+            });
+
+            res.status(200).send(rtnDataDict);
+          },
+        );
+        return;
+
+      case 'game':
+        con.query(
+          'SELECT UserID, Username, Game, Start, End FROM User natural join GameLog ORDER BY Start DESC;',
+          (err, result) => {
+            if (err) res.status(400).send({ msg: err.message });
+
+            result.forEach((element) => {
+              rtnDataArr.push({
+                userID: element.UserID,
+                username: element.Username,
+                game: element.Game,
+                start: element.Start,
+                end: element.End,
+              });
+            });
+
+            res.status(200).send(rtnDataArr);
+          },
+        );
+        return;
+
+      default:
+        return res.status(404);
+    }
   } catch (e) {
     res.status(400).send({ msg: e.message });
   }
