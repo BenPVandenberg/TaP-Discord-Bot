daef37a7b88bc7e75a62d997e3867f3c8b9d856a
Ben <ben.vandenberg@carleton.ca>
2021-01-31 13:31:48 -0500

    remove try/catch

--- routes/data.js
+++ routes/data.js
@@ -15,52 +15,48 @@ const con = mysql.createConnection({
 /* GET data listing. */
 router.get('/:file', (req, res, next) => {
   // send contents of data file
-  try {
-    const rtnDataDict = {};
-    const rtnDataArr = [];
-    switch (req.params.file) {
-      case 'play':
-        con.query(
-          'SELECT SoundName, Count(*) AS Occurrences FROM Discord_Bot.PlayLog GROUP BY SoundName;',
-          (err, result) => {
-            if (err) res.status(400).send({ msg: err.message });
-            if (result === undefined) res.status(400).send({ msg: 'DB reporting no sounds' });
+  const rtnDataDict = {};
+  const rtnDataArr = [];
+  switch (req.params.file) {
+    case 'play':
+      con.query(
+        'SELECT SoundName, Count(*) AS Occurrences FROM Discord_Bot.PlayLog GROUP BY SoundName;',
+        (err, result) => {
+          if (err) res.status(400).send({ msg: err.message });
+          if (result === undefined) res.status(400).send({ msg: 'DB reporting no sounds' });
 
-            result.forEach((element) => {
-              rtnDataDict[element.SoundName] = element.Occurrences;
+          result.forEach((element) => {
+            rtnDataDict[element.SoundName] = element.Occurrences;
+          });
+
+          res.status(200).send(rtnDataDict);
+        },
+      );
+      return;
+
+    case 'game':
+      con.query(
+        'SELECT UserID, Username, Game, Start, End FROM User natural join GameLog ORDER BY Start DESC;',
+        (err, result) => {
+          if (err) res.status(400).send({ msg: err.message });
+
+          result.forEach((element) => {
+            rtnDataArr.push({
+              userID: element.UserID,
+              username: element.Username,
+              game: element.Game,
+              start: element.Start.toLocaleString(),
+              end: element.End ? element.End.toLocaleString() : element.End,
             });
+          });
 
-            res.status(200).send(rtnDataDict);
-          },
-        );
-        return;
+          res.status(200).send(rtnDataArr);
+        },
+      );
+      return;
 
-      case 'game':
-        con.query(
-          'SELECT UserID, Username, Game, Start, End FROM User natural join GameLog ORDER BY Start DESC;',
-          (err, result) => {
-            if (err) res.status(400).send({ msg: err.message });
-
-            result.forEach((element) => {
-              rtnDataArr.push({
-                userID: element.UserID,
-                username: element.Username,
-                game: element.Game,
-                start: element.Start.toLocaleString(),
-                end: element.End ? element.End.toLocaleString() : element.End,
-              });
-            });
-
-            res.status(200).send(rtnDataArr);
-          },
-        );
-        return;
-
-      default:
-        return res.status(404);
-    }
-  } catch (e) {
-    res.status(400).send({ msg: e.message });
+    default:
+      return res.status(404);
   }
 });
 
