3158ef4ea4af03fdbc0c5a40ef8b57d694c2d991
Ben <ben.vandenberg@carleton.ca>
2021-02-01 13:15:49 -0500

    mysql2 replace mysql

--- routes/data.js
+++ routes/data.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const fs = require('fs');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const con = mysql.createPool({
   connectionLimit: 10,
