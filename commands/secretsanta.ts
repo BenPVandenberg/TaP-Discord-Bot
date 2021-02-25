233a31b94ae133623dca9cf4d995728e64fa11f3
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2021-02-24 21:40:19 -0500

    error fixs for secretsanta

--- commands/secretsanta.ts
+++ commands/secretsanta.ts
@@ -1,5 +1,6 @@
 import Discord from "discord.js";
 import assert from "assert";
+import * as channels from "../utilities/channels";
 const config = require("../config.json");
 // secretsanta.js
 // ========
@@ -18,26 +19,25 @@ module.exports = {
         assert(message.guild);
         const roleToAdd = message.guild.roles.cache.get("770707823629697025"); // secret santa role
 
-        const text_channel = message.guild.channels.cache.get(
-            "770708204409585704",
+        const text_channel = channels.toTextChannel(
+            message.guild.channels.cache.get("770708204409585704"),
         ); // secret santa text channel
 
-        // if member already has the role
         assert(message.member);
+        assert(roleToAdd);
+        // if member already has the role
         if (message.member.roles.cache.has(roleToAdd.id)) {
-            message.member.roles
-                .remove(roleToAdd)
-                .then(text_channel.send(`${message.member} has left.`));
+            message.member.roles.remove(roleToAdd).then(() => {
+                text_channel.send(`${message.member} has left.`);
+            });
         }
         // if member doesn't have the role
         else {
-            message.member.roles
-                .add(roleToAdd)
-                .then(
-                    text_channel.send(
-                        `${message.member} has joined the Secret Santa!`,
-                    ),
+            message.member.roles.add(roleToAdd).then(() => {
+                text_channel.send(
+                    `${message.member} has joined the Secret Santa!`,
                 );
+            });
         }
         message.react("👍");
     },
