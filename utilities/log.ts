88aae2ebcc591b37c316089f256d32e40cd8c09d
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2021-04-10 13:51:49 -0400

    move levelText

--- utilities/log.ts
+++ utilities/log.ts
@@ -4,6 +4,7 @@ import * as channels from "./channels";
 export const INFO = 0;
 export const WARNING = 1;
 export const ERROR = 2;
+const levelText = ["INFO", "WARNING", "ERROR"];
 
 export function getDefaultChannel(guild: Discord.Guild) {
     // returns channel "tp-bot-testing"
@@ -17,8 +18,5 @@ export function logToDiscord(
     channel: Discord.TextChannel,
     level: number = 0,
 ) {
-    // Plan on making multiple functions for each level
-    const levelText = ["INFO", "WARNING", "ERROR"];
-
     channel.send(`<@142668923660140544> ${levelText[level]}: ${message}`);
 }
