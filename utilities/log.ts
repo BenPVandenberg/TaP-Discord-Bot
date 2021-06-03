1c1c8fdd4dcf06795608e280c1ae155767bdc4c6
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2021-06-03 13:56:17 -0400

    only notify on >= Warning

--- utilities/log.ts
+++ utilities/log.ts
@@ -1,16 +1,19 @@
 import axios from "axios";
 
-export const INFO = "INFO";
-export const WARNING = "WARNING";
-export const ERROR = "ERROR";
+export const INFO = 0;
+export const DEBUG = 1;
+export const WARNING = 2;
+export const ERROR = 3;
+const levelText = ["INFO", "DEBUG", "WARNING", "ERROR"];
 
 const discord_webhook = process.env.LOG_WEBHOOK;
 
-export function logToDiscord(message: string, level: string = INFO) {
+export function logToDiscord(message: string, level: number = INFO) {
     // if no webhook then don't do anything
     if (!discord_webhook) return;
 
+    const notifyRollin = level >= WARNING ? "<@142668923660140544> " : "";
     axios.post(discord_webhook, {
-        content: `<@142668923660140544> ${level}: ${message}`,
+        content: `${notifyRollin}${levelText[level]}: ${message}`,
     });
 }
