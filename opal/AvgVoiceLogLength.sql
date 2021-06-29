6300e17b8780331d75508e9a16787a2af3735e6d
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2021-06-28 23:33:12 -0400

    sql files only used as views in opal

--- opal/AvgVoiceLogLength.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    SUM(
-        TIMESTAMPDIFF(MINUTE, Start, End)
-    ) / 
-    (select COUNT(id) FROM VoiceLog natural join VoiceSession natural join User
-	where End IS NOT NULL)
-    AS "Averge Time Spent (min)"
-FROM VoiceLog natural join VoiceSession natural join User
-where End IS NOT NULL;
