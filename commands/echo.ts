ee1d70c89b09a760ee844b9259bc682f3a17adef
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2021-08-21 02:16:49 -0400

    echo command added

--- commands/echo.ts
+++ commands/echo.ts
@@ -1,15 +1,44 @@
-import Discord from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import assert from "assert";
+import { CommandInteraction, TextChannel } from "discord.js";
 // echo.ts
 // ========
 module.exports = {
     name: "echo",
-    description: "echos the message given",
-    alias: ["say"],
     admin: true,
     requireVoice: false,
-    async execute(message: Discord.Message, args: string[]) {
-        message.delete();
-        const echoText = args.join(" ");
-        return message.channel.send(echoText);
+    data: new SlashCommandBuilder()
+        .setName("echo")
+        .setDescription("echos the message given")
+        .addStringOption((option) =>
+            option
+                .setName("message")
+                .setDescription("The message to send")
+                .setRequired(true),
+        )
+        .addChannelOption((option) =>
+            option
+                .setName("target")
+                .setDescription("Channel to echo the message to"),
+        ),
+    async execute(interaction: CommandInteraction) {
+        const message = interaction.options.getString("message");
+        assert(message, "message is required");
+        const target = interaction.options.getChannel("target");
+
+        if (target) {
+            if (target instanceof TextChannel) {
+                await target.send(message);
+            }
+        } else {
+            // no target specified, echo to current channel
+            if (interaction.channel) {
+                await interaction.channel.send(message);
+            }
+        }
+        return await interaction.reply({
+            content: "Success!",
+            ephemeral: true,
+        });
     },
 };
