0374b0021e9903f582a92ee8173a809beb2af58e
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2021-10-18 18:07:09 -0400

    No longer spam with join messages

--- empire/events/guildMemberAdd.ts
+++ empire/events/guildMemberAdd.ts
@@ -2,6 +2,10 @@ import Discord from "discord.js";
 import * as channels from "../utilities/channels";
 import * as sql from "../utilities/sql";
 
+// time of last info message sent
+let lastInfoMessageTimestamp = new Date(0);
+let lastInfoMessage: Discord.Message | undefined = undefined;
+
 export default async function onGuildMemberAdd(member: Discord.GuildMember) {
     // Send the message to a designated channel on a server
     const generalChannel = channels.toTextChannel(
@@ -17,10 +21,28 @@ export default async function onGuildMemberAdd(member: Discord.GuildMember) {
         "2. This is NOT a democracy (RIP ur free will) \n" +
         '3. "The President is not racist" -President';
 
-    // Send the message, mentioning the member
-    generalChannel.send(
-        `Welcome to the server, ${member.toString()}.\n` + infoString
-    );
+    // check if a message was sent in the last day
+    if (
+        lastInfoMessageTimestamp.getTime() > Date.now() - 86400000 &&
+        lastInfoMessage
+    ) {
+        // check if lastInfoMessage has already been edited
+        if (
+            !lastInfoMessage
+                .toString()
+                .includes("Welcome to the server everyone")
+        ) {
+            // edit old message to be inclusive of everyone
+            lastInfoMessage = await lastInfoMessage.edit(
+                `Welcome to the server everyone!\n` + infoString
+            );
+        }
+    } else {
+        // Send the message, mentioning the member
+        lastInfoMessage = await generalChannel.send(
+            `Welcome to the server, ${member.toString()}.\n` + infoString
+        );
+    }
 
     // Add the member to the database
     await sql.verifyUser(member);
